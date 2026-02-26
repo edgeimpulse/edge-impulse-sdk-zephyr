9baade21740e18e8c2506ec61eecda227eb1c419
Edge Impulse Firmware Uploader <149399868+edgeimpulse-firmware-uploader[bot]@users.noreply.github.com>
2026-02-26 13:41:10 +0000

    SDK version: v1.87.0

--- edge-impulse-sdk/porting/zephyr/ei_sdk_version.h
+++ edge-impulse-sdk/porting/zephyr/ei_sdk_version.h
@@ -1,6 +1,6 @@
 /* The Clear BSD License
  *
- * Copyright (c) 2025 EdgeImpulse Inc.
+ * Copyright (c) 2026 EdgeImpulse Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,92 +32,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "../ei_classifier_porting.h"
-#if EI_PORTING_ARDUINO == 1
+#ifndef _EI_SDK_VERSION_H_
+#define _EI_SDK_VERSION_H_
 
-#include <Arduino.h>
-#include <stdarg.h>
-#include <stdlib.h>
+#define EI_SDK_VERSION_MAJOR             1
+#define EI_SDK_VERSION_MINOR             87
+#define EI_SDK_VERSION_PATCH             0
 
-#define EI_WEAK_FN __attribute__((weak))
-
-EI_WEAK_FN EI_IMPULSE_ERROR ei_run_impulse_check_canceled() {
-    return EI_IMPULSE_OK;
-}
-
-EI_WEAK_FN EI_IMPULSE_ERROR ei_sleep(int32_t time_ms) {
-    delay(time_ms);
-    return EI_IMPULSE_OK;
-}
-
-uint64_t ei_read_timer_ms() {
-    return millis();
-}
-
-uint64_t ei_read_timer_us() {
-    return micros();
-}
-
-void ei_serial_set_baudrate(int baudrate)
-{
-
-}
-
-EI_WEAK_FN void ei_putchar(char c)
-{
-    Serial.write(c);
-}
-
-EI_WEAK_FN char ei_getchar()
-{
-    char ch = 0;
-    if (Serial.available() > 0) {
-	    ch = Serial.read();
-    }
-    return ch;
-}
-
-/**
- *  Printf function uses vsnprintf and output using Arduino Serial
- */
-__attribute__((weak)) void ei_printf(const char *format, ...) {
-    static char print_buf[1024] = { 0 };
-
-    va_list args;
-    va_start(args, format);
-    int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
-    va_end(args);
-
-    if (r > 0) {
-#if defined(ARDUINO_ARCH_ZEPHYR)
-        Serial.write((const uint8_t *)print_buf, r);
-#else
-        Serial.print(print_buf);
 #endif
-    }
-}
-
-__attribute__((weak)) void ei_printf_float(float f) {
-    Serial.print(f, 6);
-}
-
-__attribute__((weak)) void *ei_malloc(size_t size) {
-    return malloc(size);
-}
-
-__attribute__((weak)) void *ei_calloc(size_t nitems, size_t size) {
-    return calloc(nitems, size);
-}
-
-__attribute__((weak)) void ei_free(void *ptr) {
-    free(ptr);
-}
-
-#if defined(__cplusplus) && EI_C_LINKAGE == 1
-extern "C"
-#endif
-__attribute__((weak)) void DebugLog(const char* s) {
-    ei_printf("%s", s);
-}
-
-#endif // EI_PORTING_ARDUINO == 1
