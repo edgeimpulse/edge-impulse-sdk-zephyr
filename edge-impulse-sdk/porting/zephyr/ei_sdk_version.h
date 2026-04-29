da9810755f0c3890c8200982e5b1abbc00347fdd
Edge Impulse Firmware Uploader <149399868+edgeimpulse-firmware-uploader[bot]@users.noreply.github.com>
2026-04-29 12:37:10 +0000

    SDK version: v1.92.20

--- edge-impulse-sdk/porting/zephyr/ei_sdk_version.h
+++ edge-impulse-sdk/porting/zephyr/ei_sdk_version.h
@@ -37,6 +37,6 @@
 
 #define EI_SDK_VERSION_MAJOR             1
 #define EI_SDK_VERSION_MINOR             92
-#define EI_SDK_VERSION_PATCH             19
+#define EI_SDK_VERSION_PATCH             20
 
 #endif
